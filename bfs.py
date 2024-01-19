40cde1c45027b73fa21bb99e39e2b4c41e69be09
armir <armpomor@yahoo.com>
2024-01-19 15:15:30 +0900

    final

--- bfs.py
+++ bfs.py
@@ -12,31 +12,28 @@ class BFS:
         self.queue = deque([self.start])
         self.visited = ({self.start: None})
         self.cur_node = self.start
-          
-        
+
     def bfs(self):
-        
+
         if self.queue:
             self.cur_node = self.queue.popleft()
             next_nodes = self.graph[self.cur_node]
-            
+
             for node in next_nodes:
                 if node not in self.visited:
                     self.queue.append(node)
                     self.visited[node] = self.cur_node
-              
+
         # draw BFS work
         [pg.draw.rect(self.screen, GREEN, Cell(x, y).rect) for x, y in self.visited]
         [pg.draw.rect(self.screen, GRAY, Cell(x, y).rect) for x, y in self.queue]
-        
+
         # draw path
         head = path = self.cur_node
         while path:
             pg.draw.rect(self.screen, WHITE, Cell(*path).rect, border_radius=SIZE_CELL // 3)
             path = self.visited[path]
-        
+
         # draw start and head 
         pg.draw.rect(self.screen, BLUE, Cell(*START), border_radius=SIZE_CELL // 3)
         pg.draw.rect(self.screen, MAGENTA, Cell(*head), border_radius=SIZE_CELL // 3)
-                   
-
--- main.py
+++ main.py
@@ -8,7 +8,6 @@ from dijkstra import Dijkstra
 from A_star import A_star
 from button import Button
 
-
 """Программа Лабиринт демонстрирует действия следующих алгоритмов:
 BFS, DFS, LEE, Dijkstra, A*. Чем выше вес клетки, тем она темнее на сетке с весами.
 """
@@ -21,42 +20,37 @@ class App:
         self.background = pg.image.load(f"{IMG}/image/Background.png").convert()
         self.background = pg.transform.scale(self.background, (COLS * SIZE_CELL, ROWS * SIZE_CELL))
         self.walls = Obstruction().cell_wall()  # Стены
-        self.cost = Cell().cell_cost()   # Веса
+        self.cost = Cell().cell_cost()  # Веса
         self.bfs = BFS()
         self.dfs = DFS()
         self.lee = Lee()
         self.dijkstra = Dijkstra()
         self.a_star = A_star()
-        
 
     def draw(self):
         pg.display.update()
         self.clock.tick(FPS)
         self.screen.fill(BLACK)
-        
-        
+
     def exit(self):
         for event in pg.event.get():
             if event.type == pg.QUIT:
-                exit() 
-            
-            
+                exit()
+
     def bfs_algorithim(self):
         while True:
             self.walls.draw(self.screen)
             self.bfs.bfs()
             self.draw()
             self.exit()
-            
-            
+
     def dfs_algorithim(self):
         while True:
             self.walls.draw(self.screen)
             self.dfs.dfs()
             self.draw()
             self.exit()
-            
-            
+
     def lee_algorithim(self):
         while True:
             self.walls.draw(self.screen)
@@ -64,54 +58,50 @@ class App:
             self.lee.pave_path()
             self.draw()
             self.exit()
-            
-            
+
     def a_star_algorithim(self):
         while True:
             self.cost.draw(self.screen)
             self.a_star.a_star()
             self.draw()
             self.exit()
-            
-            
+
     def dijkstra_algorithim(self):
         while True:
             self.cost.draw(self.screen)
             self.dijkstra.dijkstra()
             self.draw()
             self.exit()
-            
-            
+
     def get_font(self):
         return pg.font.Font(f'{IMG}/image/font_1.ttf', SIZE_FONT)
-    
-    
+
     def pos_button(self, index):
         return COLS * SIZE_CELL / 2, ROWS * 10 + SHIFT_BUTTON * index
-        
-                 
+
     def main_menu(self):
         pg.display.set_caption('Graph Algorithms')
         while True:
             self.screen.blit(self.background, (0, 0))
-            
+
             mouse_pos = pg.mouse.get_pos()
-            
-            bfs_button = Button(image=pg.image.load(f'{IMG}/image/0.png'), pos=self.pos_button(0), 
+
+            bfs_button = Button(image=pg.image.load(f'{IMG}/image/0.png'), pos=self.pos_button(0),
                                 text_input='BFS', font=self.get_font(), base_color=BROWN, hovering_color=WHITE)
-            dfs_button = Button(image=pg.image.load(f'{IMG}/image/0.png'), pos=self.pos_button(1), 
+            dfs_button = Button(image=pg.image.load(f'{IMG}/image/0.png'), pos=self.pos_button(1),
                                 text_input='DFS', font=self.get_font(), base_color=BROWN, hovering_color=WHITE)
-            lee_button = Button(image=pg.image.load(f'{IMG}/image/0.png'), pos=self.pos_button(2), 
+            lee_button = Button(image=pg.image.load(f'{IMG}/image/0.png'), pos=self.pos_button(2),
                                 text_input='Lee', font=self.get_font(), base_color=BROWN, hovering_color=WHITE)
-            a_star_button = Button(image=pg.image.load(f'{IMG}/image/0.png'), pos=self.pos_button(3), 
-                                text_input='A*', font=self.get_font(), base_color=BROWN, hovering_color=WHITE)
-            dijkstra_button = Button(image=pg.image.load(f'{IMG}/image/0.png'), pos=self.pos_button(4), 
-                                text_input='Dijkstra', font=self.get_font(), base_color=BROWN, hovering_color=WHITE)
-            
+            a_star_button = Button(image=pg.image.load(f'{IMG}/image/0.png'), pos=self.pos_button(3),
+                                   text_input='A*', font=self.get_font(), base_color=BROWN, hovering_color=WHITE)
+            dijkstra_button = Button(image=pg.image.load(f'{IMG}/image/0.png'), pos=self.pos_button(4),
+                                     text_input='Dijkstra', font=self.get_font(), base_color=BROWN,
+                                     hovering_color=WHITE)
+
             for button in [bfs_button, dfs_button, lee_button, a_star_button, dijkstra_button]:
                 button.change_color(mouse_pos)
                 button.update(self.screen)
-            
+
             for event in pg.event.get():
                 if event.type == pg.QUIT:
                     exit()
@@ -126,9 +116,8 @@ class App:
                         self.a_star_algorithim()
                     if dijkstra_button.check_for_input(mouse_pos):
                         self.dijkstra_algorithim()
-                    
-            self.draw()
 
+            self.draw()
 
 
 if __name__ == '__main__':
